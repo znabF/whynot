fa109a5384cda8a37d0972bea917fdc96a2d74be
znabF <119473554+znabF@users.noreply.github.com>
2023-06-09 13:50:01 -0700

    Create factorialProgram.java

---
+++ factorialProgram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter a number to compute the factorial of:");
+  // capturing what the user types
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    
+    int number = Integer.parseInt(input);
+    
+    int factorial = number;
+    int i = number;
+    while(i <= 0){
+      int lessThan = i - 1;
+      factorial = factorial * lessThan;
+      i--;
+    }
+      
+    System.out.println(factorial);
+  
+    
+  }
+}
