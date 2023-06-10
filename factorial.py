04920f70a43a1c3c7aeb806be8540b66ff5740dd
znabF <119473554+znabF@users.noreply.github.com>
2023-06-10 21:26:49 +0000

    new edits

---
+++ factorial.py
@@ -0,0 +1,15 @@
+
+def main():
+    number = int(input("Enter a number to compute the factorial of: "))
+
+    i = number
+    factorial = number
+    while(not i = 0):
+        lessThan = i - 1
+        if(lessThan == 0):
+            break
+        factorial = i * lessThan
+        i = i - 1
+    return factorial
+
+print(main())
