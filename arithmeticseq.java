6e5e6d8bb234c93c405f3adff18ce0b11e4a3ee9
znabF <119473554+znabF@users.noreply.github.com>
2023-06-11 21:53:53 +0000

    editing

--- arithmeticseq.java
+++ arithmeticseq.java
@@ -1,13 +1,28 @@
 import java.util.*;
-class Main{
+class Arithmetic{
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter starting number of sequence: ");
-    String startNumber = sc.nextLine();
+    String start = sc.nextLine();
     System.out.println("Enter the # of terms: ");
-    String numOfTerms = sc.nextLine();
+    String terms = sc.nextLine();
     System.out.println("Enter common difference: ");
-    String cd = sc.nextLine();
+    String difference = sc.nextLine();
+    
+    sc.close();
+
+    int startNumber = Integer.parseInt(start);
+    int numOfTerms = Integer.parseInt(terms);
+    int cd = Integer.parseInt(difference);
+    int[] sequence = new int[numOfTerms];
+    sequence[0] = startNumber;
+    for(int i = 1; i < numOfTerms + 1; i++){
+        sequence[i] = startNumber + cd;
+    }
+    
+    for(int i = 0; i < sequence.length; i++){
+        System.out.println(sequence[i]);
+    }
     
 }
 }
