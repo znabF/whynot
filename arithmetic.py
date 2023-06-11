3c7286626545080937b31e96ad86e77ce14f559f
znabF <119473554+znabF@users.noreply.github.com>
2023-06-11 22:33:28 +0000

    never

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,17 @@
-def generate(start, commonDifference, numOfTerms):
-    sequence = []
-    sequence[0] = start
+def generate(start, common_difference, numOfTerms):
+    sequence = [numOfTerms]
 
-    for i in range(numOfTerms):
+    for i in range(numOfTerms + 1):
         previous = i - 1
-        sequence[i] = start + commonDifference * previous    
-    print(sequence)
+        if previous < 0:
+            sequence[i] = start
+        elif previous == 0:
+            sequence[i] = start + common_difference
+        else:
+            sequence[i] = start + common_difference * previous
+    
+    print("Sequence:", end="")
+    for i in range(numOfTerms):
+        print(sequence[i], end="")
 
 generate(5, 6, 5)
