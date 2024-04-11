936daecfcd4fbf76ce7b4ac6b03424b5a7bfe4c4
Zainab Fatima <zainabfatima@zainabs-mbp.lan>
2024-04-10 20:48:13 -0700

    geo_seq

--- geometric.py
+++ geometric.py
@@ -1,2 +1,25 @@
 def generate():
-    pass
+    start = int(input("Start of sequence: "))
+    numOfTerms = int(input("# of terms: "))
+    common_ratio = int(input("Factor to multiply by: "))
+
+    geo_seq = []
+
+    for i in range(numOfTerms):
+        previous = i-1
+        # if i = 0
+        if previous < 0:
+            geo_seq.append(start)
+        # second term
+        elif previous == 0:
+            new = start * common_ratio
+            geo_seq.append(new)
+        else:
+            start * (common_ratio**i)
+    
+    print("Geometric Sequence: ", end="")
+    for i in range(numOfTerms):
+        print(geo_seq[i], end=" ")
+    print()
+generate()
+    
