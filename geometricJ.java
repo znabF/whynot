d22d2978f693520d7677acb32ce8596ce27ddd34
Zainab Fatima <zainabfatima@zainabs-mbp.lan>
2024-04-26 15:23:54 -0700

    h

--- geometricJ.java
+++ geometricJ.java
@@ -13,14 +13,6 @@ class Geo {
       System.out.println("Enter the number of terms you want in the sequence:");
       int numTerms = scanner.nextInt();
 
-
-        // Integer conversions
-        double startNumber = Double.parseDouble(start);
-        double numOfTerms = Double.parseDouble(terms);
-        double commonRatio = Double.parseDouble(common);
-
-        int length = (int) numOfTerms;
-
         double[] geoSeq = new double[length];
 
         // geoSeq[0] = startNumber;
@@ -30,11 +22,6 @@ class Geo {
             System.out.print(termTrak + " ");
             // if this is second iteration is multiples then sets the multiplied value equal to it so each iteration a termTrak will be printed.
             term *= commonRatio; 
-        }
-
-        System.out.println("Geometric Sequence: ");
-        for (int j = 0; j < length; j++) {
-            System.out.print(geoSeq[j] + " ");
         }
     }
 }
